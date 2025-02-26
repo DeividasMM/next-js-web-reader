e64adee168ce58d260960a2e35f91aec9cb04e6f
ozelos <99196325+ozelos@users.noreply.github.com>
2025-02-26 09:59:53 +0200

    Create home.js

---
+++ views/JS/home.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#signInBtn").click(function() {
+      alert("Sign In clicked!");
+    });
+  
+    $("#uploadBtn").click(function() {
+      alert("Upload clicked!");
+    });
+  
+    $("#libraryBtn").click(function() {
+      alert("Library clicked!");
+    });
+  
+    $("#commentBtn").click(function() {
+      alert("Comment page clicked!");
+    });
+  });
