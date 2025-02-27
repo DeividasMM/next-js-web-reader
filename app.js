00ecfd0c8a3fc3b39fb243337c991f9aa52db3c1
elvischepin-github <hello.elvis@icloud.com>
2025-02-28 00:03:25 +0200

    Rework of structure and partials, minor clean-up

--- app.js
+++ app.js
@@ -11,33 +11,39 @@ app.use(express.urlencoded({ extended: true }));
 
 // ROUTE
 app.get("/", (req, res) => {
-  res.render("pages/home");
+  res.render("layout", { content: "pages/home" });
 });
 
 app.get("/login", (req, res) => {
-  res.render("pages/login");
+    res.render("layout", { content: "pages/login" });
+
 });
 
 app.get("/upload", (req, res) => {
-  res.render("pages/upload");
+    res.render("layout", { content: "pages/upload" });
+
 });
 
 app.get("/edit", (req, res) => {
-  res.render("pages/edit");
+    res.render("layout", { content: "pages/edit" });
+
 });
 
 app.get("/library", (req, res) => {
-  res.render("pages/library");
+    res.render("layout", { content: "pages/library" });
+
 });
 
 // Bus su ID
 app.get("/reading", (req, res) => {
-  res.render("pages/reading");
+    res.render("layout", { content: "pages/reading" });
+
 });
 
 // Bus su ID
 app.get("/review", (req, res) => {
-  res.render("pages/review");
+    res.render("layout", { content: "pages/review" });
+
 });
 
 // SERVER
