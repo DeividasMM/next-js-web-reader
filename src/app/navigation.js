65beababf93b1833f86be8b1ca0e330025a52e78
elvischepin-github <hello.elvis@icloud.com>
2025-03-03 21:11:07 +0200

    Clerk integration with some bugs which I will take care of later

--- src/app/navigation.js
+++ src/app/navigation.js
@@ -1,19 +1,36 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Navigation() {
   return (
     <>
       <nav>
-        <button>
-          <i>Sign in</i>
-        </button>
-        <button>
-          <i>Upload File</i>
-        </button>
-        <button>
-          <i>Library</i>
-        </button>
-        <button>
-          <i>Comments</i>
-        </button>
+        <ClerkProvider>
+          <SignedOut>
+            <SignInButton />
+            {/* <SignUpButton /> */}
+          </SignedOut>
+
+          <button>
+            <i>Upload File</i>
+          </button>
+          <button>
+            <i>Library</i>
+          </button>
+          <button>
+            <i>Comments</i>
+          </button>
+
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </ClerkProvider>
       </nav>
     </>
   );
