1601873a3dc16ba0c34328a6e69a6efc62383772
DeividasMM <deividas.miliauskas2013@gmail.com>
2025-03-05 17:24:15 +0200

    70% uzbaigtas Welcome page

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,3 +1,17 @@
 export default function Footer() {
-  return <h5>&copy; 2025 All Rights Reserved</h5>;
+  return (
+    <>
+      <div className="container-footer">
+        <div className="shapedividers_com-7554"></div>
+        <div className="footer-info">
+          <ul>
+            <h4>Support</h4>
+            <li>Read-Y@gmail.com</li>
+            <li>Vilnius</li>
+          </ul>
+        </div>
+        <h5>&copy; 2025 All Rights Reserved</h5>
+      </div>
+    </>
+  );
 }
