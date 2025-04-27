b1457d48ca16491d48a5d31c318244eef3b970f9
Lukas Matulis <lukas.matulis@vvk.lt>
2025-04-27 19:42:27 +0300

    delete button

--- src/app/card.js
+++ src/app/card.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Card({ id, title, author }) {
+export default function Card({ id, title, author, onDelete }) {
   return (
     <>
-      <div className="card">
+      <div className="card" style={{ position: "relative" }}>
         <div className="content-box">
           <h3>{title}</h3>
           <p>{author}</p>
@@ -11,6 +11,17 @@ export default function Card({ id, title, author }) {
         <Link href={`/library/${id}/reading`} className="reading-link">
           Start to read!
         </Link>
+        <button
+          onClick={() => onDelete(id)}
+          style={{
+            position: "absolute",
+            top: "10px",
+            padding: "5px",
+            cursor: "pointer"
+          }}
+        >
+          Delete
+        </button>
       </div>
     </>
   );
